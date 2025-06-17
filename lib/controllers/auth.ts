1acc387462dee369596002256ba3be8e3f6032ee
Gonza <gonza.cunet@outlook.com>
2025-06-17 12:21:59 -0300

    third commit

---
+++ lib/controllers/auth.ts
@@ -0,0 +1,33 @@
+import { User } from "lib/user";
+import { Auth } from "lib/auth";
+import { addMinutes } from "date-fns/addMinutes";
+import gen from "random-seed";
+
+var seed = "unapruebita";
+var random = gen.create(seed);
+export async function findOrCreateAuth(email: string): Promise<Auth> {
+  const cleanEmail = email.trim().toLowerCase();
+  const auth = await Auth.findByEmail(cleanEmail);
+  if (auth) {
+    return auth;
+  } else {
+    const newUser = await User.createNewUser({ email: cleanEmail });
+    const newAuth = await Auth.createNewAuth({
+      email: cleanEmail,
+      userId: newUser.ref.id,
+      code: "",
+      expires: new Date(),
+    });
+  }
+  return auth;
+}
+export async function sendCode(email: string) {
+  const auth = await findOrCreateAuth(email);
+  const code = random.intBetween(10000, 99999);
+  const now = new Date();
+  const twentyMinutesFromNow = addMinutes(now, 20);
+  auth.data.code = code;
+  auth.data.expires = twentyMinutesFromNow;
+  await auth.push();
+  return auth;
+}
--- pages/api/index.ts
+++ pages/api/index.ts
@@ -1,6 +1,10 @@
 import type { NextApiRequest, NextApiResponse } from "next";
-import {} from "lib/firestore";
+import { Auth } from "lib/auth";
+import { User } from "lib/user";
+import { findOrCreateAuth } from "lib/controllers/auth";
+import { sendCode } from "lib/controllers/auth";
 
-export default function (req: NextApiRequest, res: NextApiResponse) {
-  res.send("hola soy la api" + " " + process.env.FIREBASE_CONNECTION);
+export default async function (req: NextApiRequest, res: NextApiResponse) {
+  const auth = await sendCode(req.body.email);
+  res.send(auth);
 }
